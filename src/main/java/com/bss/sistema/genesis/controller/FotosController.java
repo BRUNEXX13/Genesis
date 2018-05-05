2dbc666c89b54d446ad9724623fd39b87043031d
Bruno_Santos <brunocontato2012@gmail.com>
2018-05-05 01:49:45 -0300

    UPLAOD_REMOVE_FOTO_USUARIO

--- src/main/java/com/bss/sistema/genesis/controller/FotosController.java
+++ src/main/java/com/bss/sistema/genesis/controller/FotosController.java
@@ -1,6 +1,8 @@
 package com.bss.sistema.genesis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,5 +30,11 @@ public class FotosController {
 
 		return resultado;
 	}
+	//Recuperando a foto e Lendo a url da foto//
+	@GetMapping("temp/{nome:.*}")
+	public byte[] recuperarFotoTemporaria(@PathVariable String nome) {
+		return fotoStorage.recuperarFotoTemporaria(nome);
+	}
+	
 
 }
