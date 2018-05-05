9a6d7ef5c22f58c1ca49af9661d01ed4aae3c2bf
Bruno_Santos <brunocontato2012@gmail.com>
2018-05-04 22:21:00 -0300

    CRIADO_PASTA_DIRETORIO
    CRIADO_PASTA_DIRETORIO

---
+++ src/main/java/com/bss/sistema/genesis/storage/local/FotoStorageLocal.java
@@ -0,0 +1,54 @@
+package com.bss.sistema.genesis.storage.local;
+
+import static java.nio.file.FileSystems.getDefault;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.bss.sistema.genesis.storage.FotoStorage;
+
+public class FotoStorageLocal implements FotoStorage {
+
+	private static final Logger logger = LoggerFactory.getLogger(FotoStorageLocal.class);
+	
+	private Path local;
+	private Path localTemporario;
+	
+	// IMPLEMENTAR A PASTA NO WINDOWS SEMPRE getPROPRETY. user.home //
+	public FotoStorageLocal() {
+		this(getDefault().getPath(System.getProperty("user.home"), ".genesisfotos"));
+	}
+	
+	public FotoStorageLocal(Path path) {
+		System.out.println("PATH --------------->" + path.toString());
+		this.local = path;
+		criarPastas();
+	}
+
+	private void criarPastas() {
+		try {
+			Files.createDirectories(this.local);
+			this.localTemporario = getDefault().getPath(this.local.toString(), "temp");
+			Files.createDirectories(this.localTemporario);
+			
+			if (logger.isDebugEnabled()) {
+				logger.debug("Pastas criadas para salvar fotos.");
+				logger.debug("Pasta default: " + this.local.toAbsolutePath());
+				logger.debug("Pasta temporária: " + this.localTemporario.toAbsolutePath());
+			}
+		} catch (IOException e) {
+			throw new RuntimeException("Erro criando pasta para salvar foto", e);
+		}
+	}
+
+	@Override
+	public void salvarTemporariamente(MultipartFile[] files) {
+		System.out.println(">>>> salvando a foto temporariamente...");
+	}
+	
+}
